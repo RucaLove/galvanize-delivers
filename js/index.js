0e08c8f00acdebc37a4a65a38cff9a21e9af7407
Haley Kalb <haleykalb@Haleys-MacBook-Air.local>
2017-02-14 08:43:48 -0700

    start assignment

---
+++ js/index.js
@@ -0,0 +1,16 @@
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
+
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
