e5c750b0423d9d1c0cd7791865ed0ff5d7ff89c3
Haley Kalb <haleykalb@Haleys-MacBook-Air.local>
2017-02-15 19:13:22 -0700

    add button function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
-
-  </body>
-</html>
-
-
 (function($){
   $(function(){
+    console.log("here");
 
-    $('.button-collapse').sideNav();
+    //$('.button-collapse').sideNav();
+
+        $('#burger').click(function(){
+          console.log('burger clicks');
+          var inputVal = 'test';
+          console.log(inputVal);
+          console.log($('tr'));
+          $('tr')[1].append($('HELP'));
+        })
+
+        // $('#burger').click(function(){
+        //   console.log('burger clicks');
+        //   var inputVal = 'test';
+        //   console.log(inputVal);
+        //   $('tr').append('<td id="burger">' + inputVal +'</td>');
+        // })
+
 
   }); // end of document ready
+
 })(jQuery); // end of jQuery name space
